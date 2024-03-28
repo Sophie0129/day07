ae1ad28e41ec1b73dad081fdabe1acdc38eae4f5
minseok <heekms6@naver.com>
2024-03-28 16:54:29 +0900

    js,jquery

---
+++ ex29.js
@@ -0,0 +1,25 @@
+testObj = {
+    key : '값',
+    key1 : '값1',
+    test : () => {
+        alert('new test');
+    },
+    test1 : () => {
+        alert('new test111');
+    },
+    num : 123
+} //객체 만들기
+
+/*
+let num = 123;
+
+const test = () => 
+{alert('test');}
+
+const test1 = () => 
+{alert('test111');}
+*/
+
+/*파일명이 js일 경우 자바스크립트의 내용을
+따로 파일로 만드는것, 
+<script></script>를 적을 필요가 없다*/
---
+++ ex29_2.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    alert(2222)
+}
+
+ex_1 = {
+    test : function(){
+        alert('ex_1')
+    }
+}
